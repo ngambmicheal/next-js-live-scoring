c70d0036aedbaafbaf36155471a7a0294560e6b7
Yossa Michel <ngambmicheal@gmail.com>
2021-09-24 16:58:32 +0100

    added layout

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,7 +1,15 @@
 import Link from 'next/link';
+import Layout from '../components/layout';
 
 export default function firstPost(){
     return <> 
-        This is the first Page <Link href='/posts/next-post'><a>Click here to continue</a></Link>
+        <Layout>
+            <style jsx>
+                {`a{
+                    color:red
+                }`}
+            </style>
+            This is the first Page <Link href='/posts/next-post'><a>Click here to continue</a></Link>
+        </Layout>
     </>
 }
