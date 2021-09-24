b09cbc394609527dbcc5d75e4adf91c7ad56e3ac
Yossa Michel <ngambmicheal@gmail.com>
2021-09-24 11:29:09 +0100

    update next-post

--- pages/posts/next-post.jsx
+++ pages/posts/next-post.jsx
@@ -1,5 +1,16 @@
 import Link from 'next/link'; 
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function nextPost(){
-    return <> This is the next Post <Link href='/posts/first-post'>Back to first Post</Link></>
+    return <> 
+        <Head>
+            <title> This is the last Post </title>
+        </Head>
+        This is the next Post <Link href='/posts/first-post'>Back to first Post</Link>
+        
+        <br/> 
+
+        <Image src='/images/test.jpeg' height={160} width={164} alt='Your Name'></Image>
+    </>
 }
